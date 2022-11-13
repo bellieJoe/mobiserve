f3b94437e22fee9bc971062a3bbabd9e2a84622d
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2022-11-13 20:01:40 +0800

    Added bottom navigation bar for administrator interface

--- app/src/main/java/com/thesis/mobiserve/MainActivity.java
+++ app/src/main/java/com/thesis/mobiserve/MainActivity.java
@@ -1,14 +1,22 @@
 package com.thesis.mobiserve;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_administrator);
+        setContentView(R.layout.activity_main);
+
     }
 }
